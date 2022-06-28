f70c0d1f86c5aee69b424c5c2f57b589ffea3023
Rafael Marques <rafael.marques-98@hotmail.com>
2022-06-27 22:12:09 -0300

    Iniciando projeto Ignite feed

---
+++ 01-Fundamentos-ReactJS/src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import styles from './Sidebar.module.css'
+
+export function Sidebar(){
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1510915361894-db8b60106cb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
+      />
+
+      <div className={styles.profile}>
+        <strong>Rafael Marques</strong>
+        <span>Web developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
