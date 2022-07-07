00f0378a0d3f5f975e8d324cc0e8551e187e81cb
Rafael Marques <rafael.marques-98@hotmail.com>
2022-07-06 21:25:11 -0300

    Separando o componente avatar

--- 01-Fundamentos-ReactJS/src/components/Comment.jsx
+++ 01-Fundamentos-ReactJS/src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import styles from './Comment.module.css';
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 export function Comment(){
   return(
     <div className={styles.comment}>
-      <img src="https://images.unsplash.com/photo-1504593811423-6dd665756598?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80" alt="" />
+      <Avatar hasBorder={false} src="https://images.unsplash.com/photo-1504593811423-6dd665756598?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80" />
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
--- 01-Fundamentos-ReactJS/src/components/Sidebar.jsx
+++ 01-Fundamentos-ReactJS/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 import styles from './Sidebar.module.css'
 
 export function Sidebar(){
@@ -11,7 +12,7 @@ export function Sidebar(){
 
       <div className={styles.profile}>
 
-        <img className={styles.avatar} src="https://github.com/rafaelmfs.png" alt="" />
+        <Avatar src="https://github.com/rafaelmfs.png"/>
 
         <strong>Rafael Marques</strong>
         <span>Web developer</span>
