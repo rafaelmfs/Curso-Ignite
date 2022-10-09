3f188994bd3347be09d8da7a7104f2da5562f74f
rafaelmfs <rafael.marques-98@hotmail.com>
2022-10-09 10:35:00 -0300

    Corrigindo

--- 03-dt-money/src/hooks/useSummary.ts
+++ 03-dt-money/src/hooks/useSummary.ts
@@ -1,6 +1,5 @@
 import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../contexts/transactionsContext'
-import { useMemo } from 'react'
 
 export function useSummary() {
   const transactions = useContextSelector(TransactionsContext, (context) => {
@@ -9,26 +8,45 @@ export function useSummary() {
 
   // Sempre que o componente for renderizado as variáveis são criadas novamente e acaba ocupando um espaço diferente na memṕoria,
   // Para que ela seja criada uma única vez, podemos utilizar o hook useMemo e passar um array de dependencias para que ela altere somente se alguma informação das dependencias alterarem.
-  const summary = useMemo(() => {
-    transactions.reduce(
-      (acc, transaction) => {
-        if (transaction.type === 'income') {
-          acc.income += transaction.price
-          acc.total += transaction.price
-        } else {
-          acc.outcome += transaction.price
-          acc.total -= transaction.price
-        }
+  // const summary = useMemo(() => {
+  //   transactions.reduce(
+  //     (acc, transaction) => {
+  //       if (transaction.type === 'income') {
+  //         acc.income += transaction.price
+  //         acc.total += transaction.price
+  //       } else {
+  //         acc.outcome += transaction.price
+  //         acc.total -= transaction.price
+  //       }
 
-        return acc
-      },
-      {
-        income: 0,
-        outcome: 0,
-        total: 0,
-      },
-    )
-  }, [transactions])
+  //       return acc
+  //     },
+  //     {
+  //       income: 0,
+  //       outcome: 0,
+  //       total: 0,
+  //     },
+  //   )
+  // }, [transactions])
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price
+        acc.total += transaction.price
+      } else {
+        acc.outcome += transaction.price
+        acc.total -= transaction.price
+      }
+
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  )
 
   return summary
 }
