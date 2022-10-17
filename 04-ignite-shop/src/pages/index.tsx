4644ad7f660123eea124e4778314d096e058a226
rafaelmfs <rafael.marques-98@hotmail.com>
2022-10-17 20:19:34 -0300

    Criando estilos globais

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -1,5 +1,22 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$green500',
+  border: 0,
+  borderRadius: 4,
+  padding: '8px 6px',
+
+  span:{
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  }
+})
+
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <Button>Click-me <span>++</span></Button>
   )
 }
